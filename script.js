cf17b55b2f9c7e7374123f3f9870738b229ed2ba
Alexandr Kruzhanovskyi <alexandrkruzhanovskyi@MacBook-Pro-Alexandr.local>
2023-05-23 18:12:13 +0300

    calculatorOfSum

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculateSum() {
+    let input1 = Number(document.getElementById("input1").value);
+    let input2 = Number(document.getElementById("input2").value);
+    let input3 = Number(document.getElementById("input3").value);
+  
+    let sum = input1 + input2 + input3;
+  
+    document.getElementById("sumInput").value = sum;
+  }
